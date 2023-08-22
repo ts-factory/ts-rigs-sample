dbea6d564289f0179359ecb6a7cab78507a6481c
Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
2023-08-22 10:43:01 +0300

    add Bublik configuration example
    Bublik Web application provides an interface to testing results
    analysis.
    
    Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

---
+++ bublik/references.py
@@ -0,0 +1,51 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2023 OKTET Labs Ltd. All rights reserved.
+
+class References:
+    logs = {
+        "LOGS_BASE": {
+            "name": "TS-Factory logs base",
+            "uri": [
+                "https://ts-factory.io/logs/"
+            ],
+        },
+        "dpdk-bz": {
+            "name": "DPDK.org Bugzilla",
+            "uri": [
+                "https://bugs.dpdk.org/show_bug.cgi?id="
+            ],
+        },
+    }
+
+    revisions = {
+        "DPDK_ETHDEV_TS_REV": {
+            "name": "DPDK-EthDev-TS",
+            "uri": [
+                "https://github.com/ts-factory/dpdk-ethdev-ts"
+            ],
+        },
+        "NET_DRV_TS_REV": {
+            "name": "Net-Drv-TS",
+            "uri": [
+                "https://github.com/ts-factory/net-drv-ts"
+            ],
+        },
+        "TSCONF_REV": {
+            "name": "TS-Conf",
+            "uri": [
+                "https://github.com/ts-factory/ts-conf"
+            ],
+        },
+        "TSRIGS_REV": {
+            "name": "TS-Rigs",
+            "uri": [
+                "https://github.com/ts-factory/ts-rigs-sample"
+            ],
+        },
+        "TE_REV": {
+            "name": "Test Environment",
+            "uri": [
+                "https://github.com/ts-factory/test-environment"
+            ],
+        },
+    }
