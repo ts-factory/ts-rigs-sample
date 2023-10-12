28990d6b960e027f563d853113e93f6e8af51f83
Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
2023-10-12 09:36:54 +0300

    move metadata generation from Jenkins to TS and Dispatcher
    Meta data are essential for logs import to Bublik.
    Meta data generation has been moved from Jenkins to TS run.sh and
    dispatcher.sh to be able to import manual runs.
    
    Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

--- jenkins/defs/dpdk-ethdev-ts/defs.groovy
+++ jenkins/defs/dpdk-ethdev-ts/defs.groovy
@@ -12,7 +12,6 @@ def set_defs(ctx) {
     ctx.TS_LOGS_SUBPATH = 'dpdk-ethdev-ts/'
 
     // Variables required for bublik-import
-    ctx.PROJECT = 'ts-factory'
     ctx.TS_BUBLIK_URL = 'https://ts-factory.io/bublik/'
     ctx.TS_LOGS_URL_PREFIX = 'https://ts-factory.io/logs/'
 
--- jenkins/defs/net-drv-ts/defs.groovy
+++ jenkins/defs/net-drv-ts/defs.groovy
@@ -12,7 +12,6 @@ def set_defs(ctx) {
     ctx.TS_LOGS_SUBPATH = 'net-drv-ts/'
 
     // Variables required for bublik-import
-    ctx.PROJECT = 'ts-factory'
     ctx.TS_BUBLIK_URL = 'https://ts-factory.io/bublik/'
     ctx.TS_LOGS_URL_PREFIX = 'https://ts-factory.io/logs/'
 
